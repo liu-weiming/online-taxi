349257561e19184306bbb9e22a1837acd0ac6576
weeming <2655834088@qq.com>
2022-09-17 16:28:24 +0800

    init project

---
+++ api-passenger/src/main/java/com/lwm/ApiPassengerApplication.java
@@ -0,0 +1,17 @@
+package com.lwm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author weeming
+ * @date 2022/9/17 15:41
+ */
+@SpringBootApplication
+public class ApiPassengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+
+}
